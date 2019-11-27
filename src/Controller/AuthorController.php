9564ac0593b142da5cded3a139afb6c8c8eff1a5
Adrien RUSSO <adrien.russo@gmail.com>
2019-11-27 11:01:55 +0100

    Commentaires à gogo !!!!!

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -24,7 +24,8 @@ class AuthorController extends AbstractController
      * @Route("/authors", name="authors")
      * @param AuthorRepository $authorRepository
      * @return Response
-     * Affiche tous les auteurs
+     *
+     * Affiche tous les auteurs de l'entité Authors
      */
     public function getAuthors(AuthorRepository $authorRepository)
     {
@@ -32,11 +33,15 @@ class AuthorController extends AbstractController
         return $this->render('authors.html.twig', ['authors' => $authors]);
     }
 
+
     /**
      * @Route("/author/show/{id}", name="author")
      * @param AuthorRepository $authorRepository
      * @param $id
      * @return Response
+     *
+     * Affiche un seul auteur de l'entité Authors,
+     * celui dont l'ID est précisé dans la WildCard
      */
     public function getAuthorById(AuthorRepository $authorRepository, $id)
     {
@@ -44,22 +49,19 @@ class AuthorController extends AbstractController
         return $this->render('author.html.twig', ['author' => $author]);
     }
 
+
     /**
      * @Route("/authors/search", name="search_author")
      * @param AuthorRepository $authorRepository
      * @param Request $request
      * @return Response
+     *
+     * Affiche les auteurs qui correspondent aux données transmises via le formulaire de recherche (en GET)
      */
     public function searchInAuthors(AuthorRepository $authorRepository, Request $request)
     {
-        /**
-         * Ajoute les valeurs du GET dans des variables puis ces variables dans un array, afin de
-         * les passer à la vue pour traiter les attributs "Value" dans la vue twig
-         */
         $get = [];
         $nameAndOrFirstName = $request->query->get('nameAndOrFirstName');
-
-
         $get['nameAndOrFirstName'] = $nameAndOrFirstName;
 
         $authors = $authorRepository->customSearch($nameAndOrFirstName);
@@ -67,12 +69,28 @@ class AuthorController extends AbstractController
 
     }
 
+
+    /**
+     * @Route("/author/new", name="author_new")
+     *
+     * Permet d'afficher la vue author_form.html.twig
+     * avec les champs du formulaire vides
+     */
+    public function newAuthor()
+    {
+        return $this->render('author_form.html.twig');
+    }
+
+
     /**
      * @Route("/author/insert", name="author_insert")
      * @param EntityManagerInterface $entityManager
      * @param Request $request
      * @return Response
      * @throws Exception
+     *
+     * Insert un auteur dans la BDD avec les informations fournies
+     * via le formulaire de la vue book_form.html.twig (en POST)
      */
     public function insertAuthor(EntityManagerInterface $entityManager, Request $request)
     {
@@ -95,40 +113,23 @@ class AuthorController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/author/new", name="author_new")
-     */
-    public function newAuthor()
-    {
-        return $this->render('author_form.html.twig');
-    }
-
-    /**
-     * @Route("/author/delete/{id}", name="author_delete")
-     * @param EntityManagerInterface $entityManager
-     * @param AuthorRepository $authorRepository
-     * @param $id
-     * @return RedirectResponse
-     */
-    public function deleteAuthor(EntityManagerInterface $entityManager, AuthorRepository $authorRepository, $id)
-    {
-        $author = $authorRepository->find($id);
-        $entityManager->remove($author);
-        $entityManager->flush();
-        return $this->redirectToRoute('authors');
-    }
 
     /**
      * @Route("/author/edit/{id}", name="author_edit")
      * @param AuthorRepository $authorRepository
      * @param $id
      * @return Response
+     *
+     * Permet d'appeler la vue author_form.html.twig
+     * préremplie avec l'auteur dont l'ID est
+     * dans la WildCard de la route
      */
     public function editAuthor(AuthorRepository $authorRepository, $id)
     {
         $author = $authorRepository->find($id);
         return $this->render('author_form.html.twig', ['author' => $author]);
     }
+
 
     /**
      * @Route("/author/update", name="author_update")
@@ -137,6 +138,10 @@ class AuthorController extends AbstractController
      * @param Request $request
      * @return RedirectResponse
      * @throws Exception
+     *
+     * Peret de prendre les données envoyées via la vue
+     * author_form.html.twig (en POST) et utilise les méthodes "setters"
+     * de l'entité Book pour modifier les données inscrites en BDD
      */
     public function updateAuthor(AuthorRepository $authorRepository, EntityManagerInterface $entityManager, Request $request)
     {
@@ -167,4 +172,27 @@ class AuthorController extends AbstractController
         $entityManager->flush();
         return $this->redirectToRoute('authors');
     }
+
+
+
+    /**
+     * @Route("/author/delete/{id}", name="author_delete")
+     * @param EntityManagerInterface $entityManager
+     * @param AuthorRepository $authorRepository
+     * @param $id
+     * @return RedirectResponse
+     *
+     * Supprime l'auteur de la BDD dont l'ID est indiqué dans la WildCard
+     */
+    public function deleteAuthor(EntityManagerInterface $entityManager, AuthorRepository $authorRepository, $id)
+    {
+        $author = $authorRepository->find($id);
+        $entityManager->remove($author);
+        $entityManager->flush();
+        return $this->redirectToRoute('authors');
+    }
+
+
+
+
 }
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,63 +6,63 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\BookRepository;
 use App\Entity\Book;
 
+/**
+ * Controlleur des Livres
+ */
 class BookController extends AbstractController
 {
     /**
      * @Route("/books", name="books")
      * @param BookRepository $bookRepository
      * @return Response
-     * Le varHint BookRepository permet de stocker dans $bookRepository une instance de la classe
-     * BookRepository. C'est dans cette classe qu'est stocké la méthode findAll().
-     * Le Repository construit les
-     * requettes SQL pour accèder au contenu de l'entité Book
+     *
+     * Affiche tous les livres de l'entité Book
      */
-
     public function getBooks(BookRepository $bookRepository)
     {
         $books = $bookRepository->findAll();
         return $this->render('books.html.twig', ['books' => $books]);
     }
 
+
     /**
      * @Route("/book/show/{id}", name="book")
      * @param BookRepository $bookRepository
      * @param $id
      * @return Response
-     * Le varHint BookRepository permet de stocker dans $bookRepository une instance de la classe
-     * BookRepository. C'est dans cette classe qu'est stocké la méthode find($id). Le Repository construit la
-     * requette SQL pour accèder au contenu de l'entité Book dont l'ID est passé dans la WildCard de la route.
+     *
+     * Affiche un seul livre de l'entité Book,
+     * celui dont l'ID est précisé dans la WildCard
      */
-
     public function getBook(BookRepository $bookRepository, $id)
     {
         $book = $bookRepository->find($id);
         return $this->render('book.html.twig', ['book' => $book]);
     }
 
+
     /**
      * @Route("/books/search", name="search_books")
      * @param BookRepository $bookRepository
      * @param Request $request
      * @return Response
+     *
+     * Affiche les livres qui correspondent aux données transmises
+     * via le formulaire de recherche(en GET)
      */
     public function searchInBooks(BookRepository $bookRepository, Request $request)
     {
-        /**
-         * Ajoute les valeurs du GET dans des variables puis ces variables dans un array, afin de
-         * les passer à la vue pour traiter les attributs "Value" dans la vue twig
-         */
         $get = [];
         $style = $request->query->get('style');
         $title = $request->query->get('title');
         $stock = $request->query->get('inStock');
-
 
         $get['style'] = $style;
         $get['title'] = $title;
@@ -70,13 +70,29 @@ class BookController extends AbstractController
 
         $books = $bookRepository->customSearch($style, $title, $stock);
         return $this->render('books.html.twig', ['books' => $books, 'get' => $get]);
-
     }
+
+
+    /**
+     * @Route("/book/new", name="book_new")
+     *
+     * Permet d'afficher la vue book_form.html.twig
+     * avec les champs du formulaire vides
+     */
+    public function newBook()
+    {
+        return $this->render('book_form.html.twig');
+    }
+
 
     /**
      * @Route("/book/insert", name="book_insert")
      * @param EntityManagerInterface $entityManager
      * @param Request $request
+     * @return Response
+     *
+     * Insert un livre dans la BDD avec les informations fournies
+     * dans le formulaire de la vue book_form.html.twig(en POST)
      */
     public function insertBook(EntityManagerInterface $entityManager, Request $request)
     {
@@ -99,29 +115,16 @@ class BookController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/book/new", name="book_new")
-     */
-    public function newBook()
-    {
-        return $this->render('book_form.html.twig');
-    }
-
-    /**
-     * @Route("/book/delete/{id}", name="book_delete")
-     */
-    public function deleteBook(EntityManagerInterface $entityManager, BookRepository $bookRepository, $id)
-    {
-        $book = $bookRepository->find($id);
-        $entityManager->remove($book);
-        $entityManager->flush();
-        return $this->redirectToRoute('books');
-    }
 
     /**
      * @Route("/book/edit/{id}", name="book_edit")
+     * @param BookRepository $bookRepository
      * @param $id
      * @return Response
+     *
+     * Permet d'appeler la vue book_form.html.twig
+     * préremplie avec le livre dont l'ID est
+     * dans la WildCard de la route
      */
     public function editBook(BookRepository $bookRepository, $id)
     {
@@ -129,12 +132,17 @@ class BookController extends AbstractController
         return $this->render('book_form.html.twig', ['book' => $book]);
     }
 
+
     /**
      * @Route("/book/update", name="book_update")
+     * @param BookRepository $bookRepository
      * @param EntityManagerInterface $entityManager
      * @param Request $request
      * @return RedirectResponse
-     * @throws Exception
+     *
+     * Permet de prendre les données envoyées via la vue
+     * book_form.html.twig (en POST) et utilise les méthodes "setters"
+     * de l'entité Book pour modifier les données inscrites en BDD
      */
     public function updateBook(BookRepository $bookRepository, EntityManagerInterface $entityManager, Request $request)
     {
@@ -151,7 +159,7 @@ class BookController extends AbstractController
         $book->setStyle($style);
         $book->setNbPages($nbPages);
 
-        if ($nbPages == 1) {
+        if ($inStock == 1) {
             $book->setInStock(true);
         } else {
             $book->setInStock(false);
@@ -163,4 +171,20 @@ class BookController extends AbstractController
     }
 
 
+    /**
+     * @Route("/book/delete/{id}", name="book_delete")
+     * @param EntityManagerInterface $entityManager
+     * @param BookRepository $bookRepository
+     * @param $id
+     * @return RedirectResponse
+     *
+     * Supprime le livre de la BDD dont l'ID est indiqué dans la WildCard
+     */
+    public function deleteBook(EntityManagerInterface $entityManager, BookRepository $bookRepository, $id)
+    {
+        $book = $bookRepository->find($id);
+        $entityManager->remove($book);
+        $entityManager->flush();
+        return $this->redirectToRoute('books');
+    }
 }
